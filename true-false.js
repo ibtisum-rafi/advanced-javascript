fb50c89d67e83434810a0f424e9ed5bd612e0dac
ibtisum_rafi <ibtisum.rafi59@yahoo.com>
2020-08-04 13:07:33 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,29 @@
+// const price = 50;
+
+// if(price){
+//     console.log('condition is true');
+// }
+// else{
+//     console.log('condition is false');
+// }
+
+const price = 0;
+
+if(price){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+
+/* if we don't use any specific condition then every value or every string value in a variable without 
+0 or empty string " is a true condition.
+
+Falsy values:
+1. 0
+2. ""
+3. undefined
+4. NaN
+5. Null
+6. false
